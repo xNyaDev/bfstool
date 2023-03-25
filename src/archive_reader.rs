a9cfb12723ef3480af166428c7883a9fd782b4dc
xNyaDev <xnyadev@pm.me>
2023-03-26 00:07:20 +0100

    Add ReadError to archive_reader

--- src/archive_reader.rs
+++ src/archive_reader.rs
@@ -1,3 +1,4 @@
+use std::io;
 use std::io::{BufRead, Seek};
 
 use crate::formats::Format;
@@ -14,4 +15,31 @@ pub fn read_archive<R: BufRead + Seek>(
     _archive_format: Format,
 ) -> Box<dyn ArchiveReader> {
     todo!()
+}
+
+/// Errors that can occur while reading the archive
+pub enum ReadError {
+    /// Archive magic does not match expected value
+    InvalidMagic {
+        /// Expected magic
+        expected: u32,
+        /// Actual magic
+        got: u32,
+    },
+    /// Archive version does not match expected value
+    InvalidVersion {
+        /// Expected version
+        expected: u32,
+        /// Actual version
+        got: u32,
+    },
+    /// Archive hash size does not match expected value
+    InvalidHashSize {
+        /// Expected hash size
+        expected: u32,
+        /// Actual hash size
+        got: u32,
+    },
+    /// An IO error occurred
+    IoError(io::Error),
 }
