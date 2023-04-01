346325fce59d1b23a60609e3a29e7d2a8d414e5c
xNyaDev <xnyadev@pm.me>
2023-04-01 13:25:18 +0200

    Add RawArchive for Bfs2004a

--- src/formats/bfs2004a/mod.rs
+++ src/formats/bfs2004a/mod.rs
@@ -10,3 +10,15 @@ mod hash_table_entry;
 
 /// Amount of entries in the hash table
 pub const HASH_SIZE: usize = 0x3E5;
+
+/// Raw archive contents that can be read directly from a .bfs file or written to one
+pub struct RawArchive {
+    /// The archive header
+    pub archive_header: ArchiveHeader,
+    /// Offsets for every file header
+    pub file_header_offsets: Vec<u32>,
+    /// Stores information about the hash size and how many files with specific hash are there
+    pub hash_table: HashTable,
+    /// All [FileHeader]s
+    pub file_headers: Vec<FileHeader>,
+}
