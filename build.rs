d2d79f7ea4c80e69ebec0b2ded8fe873d98b3541
xNyaDev <xnyadev@pm.me>
2023-04-10 11:42:15 +0200

    CLI - Add windows metadata

---
+++ build.rs
@@ -0,0 +1,15 @@
+fn main() {
+    #[cfg(feature = "cli")]
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        tauri_winres::WindowsResource::new()
+            .set("FileDescription", "bfstool-cli")
+            .set(
+                "LegalCopyright",
+                "Licensed under MIT or Apache-2.0, xNyaDev 2023",
+            )
+            .set("OriginalFilename", "bfstool-cli.exe")
+            .set("ProductName", "bfstool-cli")
+            .compile(Some("bfstool-cli"))
+            .unwrap();
+    }
+}
